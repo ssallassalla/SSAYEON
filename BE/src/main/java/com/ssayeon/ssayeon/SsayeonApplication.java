d6f07337c75db4cbd39f44bf8a8135464330a1f0
윤민수 <minsoo9615@naver.com>
2022-11-03 17:12:17 +0900

    init

---
+++ BE/src/main/java/com/ssayeon/ssayeon/SsayeonApplication.java
@@ -0,0 +1,13 @@
+package com.ssayeon.ssayeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsayeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsayeonApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/ssayeon/ssayeon/SsayeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssayeon.ssayeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsayeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
