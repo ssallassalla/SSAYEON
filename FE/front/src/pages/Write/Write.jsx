39b3a560d5f9ac3bb2d97044c7925fc1d27fb71d
dailybetter <a40293865@gmail.com>
2022-11-15 01:59:24 +0900

    bug: Eslint  or prettier 관련 버그 발생

--- FE/front/src/pages/Write/Write.jsx
+++ FE/front/src/pages/Write/Write.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-
+import Editor from './Components/editors';
 export default function Write() {
-  return <div>Write</div>;
+  return (
+    <>
+    <h2>Write</h2>
+    <div>
+        <editor/>
+    </div>
+    </>
+  )
 }
