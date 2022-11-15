6cd3238fa43dcac0dcd45cb7b885a061cdc51f4f
moonthree <fpdhslr0725@gmail.com>
2022-11-15 22:16:11 +0900

    feat: 게시판 기본 구조 설정
    Json : 목업 json 파일 사용
    기능 : json파일에서 데이터 불러오는 것만 구현

--- FE/front/src/pages/Board/Board.jsx
+++ FE/front/src/pages/Board/Board.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import BOARD_MOCK_DATA from '../../data/json_test/BOARD_MOCK_DATA.json';
+import Post from '../../components/Board/Post';
 
 export default function Board() {
-  return <div>Board</div>;
+  return (
+    <div>
+      <TitleDiv>
+        {BOARD_MOCK_DATA.map(item => (
+          <Post post={item} key={item.id} />
+        ))}
+      </TitleDiv>
+    </div>
+  );
 }
+
+// start of styledComponent
+let TitleDiv = styled.div`
+  display: grid;
+  grid-template-columns: 50% 50%;
+
+  .board:nth-child(odd) {
+    border-right: 1px solid #eeeeee;
+  }
+`;
