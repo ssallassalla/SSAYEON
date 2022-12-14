491276b4dfb98b4ea23bf62bfd38b396505073aa
dailybetter <a40293865@gmail.com>
2022-12-15 02:09:39 +0900

    feat: BasicEdotr 기본 틀 개발

--- FE/front/src/pages/Write/Write.jsx
+++ FE/front/src/pages/Write/Write.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
-// import Editor from './Components/editors.js';
+import React, { useState } from 'react';
+import Editor from './Components/editors';
+import QuillEditor from './Components/QuillEditor';
+import styled from 'styled-components';
+
 export default function Write() {
+  const [content, setContent] = useState('');
+  const [files, setFiles] = useState([]);
+
+  const onEditorChange = value => {
+    setContent(value);
+    console.log(content);
+  };
+  const onFilesChange = files => {
+    setFiles(files);
+    console.log(files);
+  };
+
+  const onSubmit = event => {
+    event.preventDefault();
+    setContent('');
+  };
+
   return (
     <div>
-      <h2>Write</h2>
+      <TitleWraper>
+        <Title> Editor {files}</Title>
+      </TitleWraper>
+      <Editor
+      // placeholder="Start Posting Something"
+      // onEditorChange={onEditorChange}
+      // onFilesChange={onFilesChange}
+      />
+
+      <form onSubmit={onSubmit}>
+        <div>
+          <button onClick={onSubmit}>Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
+
+let Title = styled.h1`
+  text: bold;
+`;
+
+let TitleWraper = styled.div`
+  textalign: center;
+`;
