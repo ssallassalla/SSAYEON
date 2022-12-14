a2d223ee799a853ed6462ba36acb4e2a2b4ce4ff
dailybetter <101924720+dailybetter@users.noreply.github.com>
2022-12-15 02:12:30 +0900

    Revert "feat: BasicEdotr 기본 틀 개발"

--- FE/front/src/pages/Write/Write.jsx
+++ FE/front/src/pages/Write/Write.jsx
@@ -1,50 +1,9 @@
-import React, { useState } from 'react';
-import Editor from './Components/editors';
-import QuillEditor from './Components/QuillEditor';
-import styled from 'styled-components';
-
+import React from 'react';
+// import Editor from './Components/editors.js';
 export default function Write() {
-  const [content, setContent] = useState('');
-  const [files, setFiles] = useState([]);
-
-  const onEditorChange = value => {
-    setContent(value);
-    console.log(content);
-  };
-  const onFilesChange = files => {
-    setFiles(files);
-    console.log(files);
-  };
-
-  const onSubmit = event => {
-    event.preventDefault();
-    setContent('');
-  };
-
   return (
     <div>
-      <TitleWraper>
-        <Title> Editor {files}</Title>
-      </TitleWraper>
-      <Editor
-      // placeholder="Start Posting Something"
-      // onEditorChange={onEditorChange}
-      // onFilesChange={onFilesChange}
-      />
-
-      <form onSubmit={onSubmit}>
-        <div>
-          <button onClick={onSubmit}>Submit</button>
-        </div>
-      </form>
+      <h2>Write</h2>
     </div>
   );
 }
-
-let Title = styled.h1`
-  text: bold;
-`;
-
-let TitleWraper = styled.div`
-  textalign: center;
-`;
